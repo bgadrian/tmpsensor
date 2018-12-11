dbaeb7d89c84265314a0cf259f8664f31f5acdad
Adrian BG <adrian@coder.today>
2018-12-11 23:40:10 +0200

    arguments

--- go.mod
+++ go.mod
@@ -1,9 +1,19 @@
 module github.com/bgadrian/tempwatcher
 
 require (
+	github.com/Bowery/prompt v0.0.0-20180817134258-8a1d5376df1c // indirect
 	github.com/d2r2/go-dht v0.0.0-20181107163440-d3bfc5f811a6
 	github.com/d2r2/go-logger v0.0.0-20181002101108-49d264032daf // indirect
 	github.com/d2r2/go-shell v0.0.0-20181002100851-c94f62cb3f85 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/labstack/gommon v0.2.8 // indirect
+	github.com/mattn/go-colorable v0.0.9 // indirect
+	github.com/mattn/go-isatty v0.0.4 // indirect
+	github.com/mkideal/cli v0.0.2
+	github.com/mkideal/pkg v0.0.0-20170503154153-3e188c9e7ecc // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/prometheus/client_golang v0.9.2
+	github.com/stretchr/testify v1.2.2 // indirect
+	golang.org/x/crypto v0.0.0-20181203042331-505ab145d0a9 // indirect
+	golang.org/x/sys v0.0.0-20181205085412-a5c9d58dba9a // indirect
 )
--- main.go
+++ main.go
@@ -6,7 +6,10 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
+	"strconv"
 	"time"
+
+	"github.com/mkideal/cli"
 
 	"github.com/d2r2/go-dht"
 
@@ -14,14 +17,22 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
+type argT struct {
+	cli.Helper
+	Port     int `cli:"port" usage:"web server port"  dft:"9080"`
+	Interval int `cli:"interval" usage:"seconds interval between 2 sensor reads"  dft:"15"`
+	Pin      int `cli:"pin" usage:"web server port"  dft:"7"`
+}
+
 func main() {
-	//TODO put them as flags
-	port := "9080"
-	intervalSeconds := 30
-	dataPin := 7
+	cli.Run(new(argT), run)
+}
+
+func run(ctx *cli.Context) error {
+	args := ctx.Argv().(*argT)
+
 	sensorName := "sensor_"
 	sensorDesc := "DHT22 sensor data."
-
 	temperature, humidity := setupPrometheus(sensorName, sensorDesc)
 
 	update := func() {
@@ -30,8 +41,8 @@ func main() {
 				fmt.Printf("Panic when reading sensor: %s \n", r)
 			}
 		}()
-		fmt.Printf("Reading data from pin %d...\n", dataPin)
-		celsius, humidityPerc, _, err := dht.ReadDHTxxWithRetry(dht.DHT22, dataPin, false, 4)
+		fmt.Printf("Reading data from pin %d...\n", args.Pin)
+		celsius, humidityPerc, _, err := dht.ReadDHTxxWithRetry(dht.DHT22, args.Pin, false, 4)
 		if err != nil {
 			fmt.Printf("failed %s\n", err)
 			return
@@ -42,7 +53,7 @@ func main() {
 		fmt.Printf("got %fC and %f%%\n", celsius, humidity)
 	}
 
-	ticker := time.NewTicker(time.Second * time.Duration(intervalSeconds))
+	ticker := time.NewTicker(time.Second * time.Duration(args.Interval))
 	go func() {
 		update()
 		for range ticker.C {
@@ -50,7 +61,7 @@ func main() {
 		}
 	}()
 
-	server := setupWebServer(port)
+	server := setupWebServer(args.Port)
 
 	fmt.Println("Press CTRL-C to exit")
 	quit := make(chan os.Signal, 1)
@@ -64,19 +75,21 @@ func main() {
 	if err != nil {
 		fmt.Println(err)
 	}
+	return nil
 }
 
-func setupWebServer(port string) *http.Server {
+func setupWebServer(port int) *http.Server {
 	router := http.NewServeMux()
 	router.Handle("/metrics", promhttp.Handler())
+	portAsString := strconv.Itoa(port)
 	server := &http.Server{
-		Addr:         ":" + port,
+		Addr:         ":" + portAsString,
 		Handler:      router,
 		ReadTimeout:  5 * time.Second,
 		WriteTimeout: 10 * time.Second,
 		IdleTimeout:  15 * time.Second,
 	}
-	fmt.Printf("Listening on http://localhost:%s/metrics\n", port)
+	fmt.Printf("Listening on http://localhost:%s/metrics\n", portAsString)
 
 	go func() {
 		httpError := server.ListenAndServe()
